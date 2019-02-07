20b7711d90f23621f2698f6f3286bce54a83706f
Maria Kukharchuk <maria.kukharchuk@itrexgroup.com>
2019-02-07 17:56:02 +0300

    Setup

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    'parser': "babel-eslint",
+    "plugins": [
+        "react"
+      ],
+    "extends": "airbnb-base",
+    "env": {
+        "browser": true,
+    }
+};
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: './src/index.html',
+  filename: './index.html',
+});
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-eslint', 'eslint-loader'],
+      },
+
+    ],
+  },
+  plugins: [htmlPlugin],
+};
